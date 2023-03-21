19a45b0cd2206cb9208eda2ede839f1e062bead3
Nikita Starostin <starostin.450503@gmail.com>
2023-03-21 11:24:23 +0300

    fix ipr2 - draw all required graphics, fix issues with logic

--- src/ipr2/ipr2.tsx
+++ src/ipr2/ipr2.tsx
@@ -4,14 +4,19 @@ import { Height } from '../constants';
 import { Ipr2Utils } from './ipr2.utils';
 
 
-function draw(task1Ref: React.MutableRefObject<HTMLDivElement | null>, task2Points: number[][]) {
+function draw(
+  task1Ref: React.MutableRefObject<HTMLDivElement | null>,
+  task2Points: number[][],
+  yLabel: string,
+  yDomain = [ 0, 200 ]
+) {
   functionPlot({
     target: task1Ref.current!,
     width: 1400,
     height: Height,
     yAxis: {
-      domain: [ 0, 200 ],
-      label: 'Error'
+      domain: yDomain,
+      label: yLabel
     },
     xAxis: {
       domain: [ 0, 200 ],
@@ -29,25 +34,47 @@ function draw(task1Ref: React.MutableRefObject<HTMLDivElement | null>, task2Poin
 
 export default function Ipr2() {
   const [ ipr2Utils ] = useState(new Ipr2Utils());
-  const task2Ref = useRef<HTMLDivElement | null>(null);
-  const task3Ref = useRef<HTMLDivElement | null>(null);
+  const task2Skz1Ref = useRef<HTMLDivElement | null>(null);
+  const task2Skz2Ref = useRef<HTMLDivElement | null>(null);
+  const task2AmplitudeRef = useRef<HTMLDivElement | null>(null);
+  const task3Skz1Ref = useRef<HTMLDivElement | null>(null);
+  const task3Skz2Ref = useRef<HTMLDivElement | null>(null);
+  const task3AmplitudeRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const mValues = ipr2Utils.getMArray();
     const errors2 = ipr2Utils.taskSecond();
     const errors3 = ipr2Utils.taskThird();
 
-    const task2Points = mValues.map((p, i) => ([ p, errors2[i] ]));
-    const task3Points = mValues.map((p, i) => ([ p, errors3[i] ]));
-    draw(task2Ref, task2Points);
-    draw(task3Ref, task3Points);
+    const task2Skz1Points = mValues.map((p, i) => ([ p, errors2[i].skz1Error ]));
+    const task2Skz2Points = mValues.map((p, i) => ([ p, errors2[i].skz2Error ]));
+    const task2AmplitudePoints = mValues.map((p, i) => ([ p, errors2[i].amplitude ]));
+    const task3Skz1Points = mValues.map((p, i) => ([ p, errors3[i].skz1Error ]));
+    const task3Skz2Points = mValues.map((p, i) => ([ p, errors3[i].skz2Error ]));
+    const task3AmplitudePoints = mValues.map((p, i) => ([ p, errors3[i].amplitude ]));
+    draw(task2Skz1Ref, task2Skz1Points, 'skz1', [ -0.5, 0.5 ]);
+    draw(task2Skz2Ref, task2Skz2Points, 'skz2', [ -0.5, 0.5 ]);
+    draw(task2AmplitudeRef, task2AmplitudePoints, 'Amplitude');
+    draw(task3Skz1Ref, task3Skz1Points, 'skz1', [ -0.5, 0.5 ]);
+    draw(task3Skz2Ref, task3Skz2Points, 'skz2', [ -0.2, 0.8 ]);
+    draw(task3AmplitudeRef, task3AmplitudePoints, 'Amplitude');
   }, []);
 
   return <>
     <h1>Ipr2 - скролить вниз, чтобы увидеть все графики</h1>
     <h2>Задание 2</h2>
-    <div ref={task2Ref} />
+    <h3>СКЗ 1</h3>
+    <div ref={ task2Skz1Ref }/>
+    <h3>СКЗ 2</h3>
+    <div ref={ task2Skz2Ref }/>
+    <h3>Амплитуда</h3>
+    <div ref={ task2AmplitudeRef }/>
     <h2>Задание 3</h2>
-    <div ref={task3Ref} />
+    <h3>СКЗ 1</h3>
+    <div ref={ task3Skz1Ref }/>
+    <h3>СКЗ 2</h3>
+    <div ref={ task3Skz2Ref }/>
+    <h3>Амплитуда</h3>
+    <div ref={ task3AmplitudeRef }/>
   </>;
 }
--- src/ipr2/ipr2.utils.ts
+++ src/ipr2/ipr2.utils.ts
@@ -1,6 +1,6 @@
 export class Ipr2Utils {
   f(n: number, N: number, fi = 0): number {
-    return Math.sin(2 * Math.PI * n / N) + fi;
+    return Math.sin(2 * Math.PI * n / N + fi);
   }
 
   getMArray(N = 64): number[] {
@@ -17,21 +17,21 @@ export class Ipr2Utils {
 
   taskSecond(N = 64, fi = 0) {
     const MArray = this.getMArray(N);
-    MArray.forEach(M => {
+    const errors = MArray.map(M => {
       let squareSum = 0;
-      let fSum = 0;
+      let xValuesSum = 0;
       let sinSum = 0;
       let cosSum = 0;
       for(let n = 0; n < M; n++) {
-        const f = this.f(n, N, fi);
-        squareSum += Math.pow(f, 2);
-        fSum += f;
-        sinSum += f * Math.sin(2 * Math.PI * n / MArray.length);
-        cosSum += f * Math.cos(2 * Math.PI * n / MArray.length);
+        const xFromI = this.f(n, N, fi);
+        squareSum += Math.pow(xFromI, 2);
+        xValuesSum += xFromI;
+        sinSum += xFromI * Math.sin(2 * Math.PI * n / M);
+        cosSum += xFromI * Math.cos(2 * Math.PI * n / M);
       }
 
       const skz1 = Math.sqrt(squareSum / M);
-      const skz2 = Math.sqrt(squareSum / M - Math.pow(fSum / M, 2))
+      const skz2 = Math.sqrt(squareSum / M - Math.pow(xValuesSum / M, 2))
       const amplitude = Math.sqrt(Math.pow(sinSum, 2) + Math.pow(cosSum, 2));
       const skz1Error = 0.707 - skz1;
       const skz2Error = 0.707 - skz2;
@@ -40,7 +40,7 @@ export class Ipr2Utils {
       return { skz1, skz2, amplitude, skz1Error, skz2Error, amplitudeError }
     });
 
-    return MArray;
+    return errors;
   }
 
   taskThird(N = 64) {
